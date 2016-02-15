6b1d3f1a10d6c2b7f422ee8436b4931efd6df7e1
Williamso <womayuku@gmail.com>
2016-02-15 21:45:24 +0100

    Initialized the draggable tiles

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,21 @@
 /**
  * Created by Williamso on 15-Feb-16.
  */
+$(function() {
 
-var win = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36];
+    var gameTiles = $('td > div');
+    gameTiles.addClass('gameTiles');
+    var win = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36];
 
+    //INITIALIZE THE DRAGGABLES
+    gameTiles.draggable();
+    gameTiles.draggable('option', 'opacity', 0.35);
+    gameTiles.draggable('option', 'delay', 500);
+
+
+    gameTiles.on('dragcreate', function() {
+        gameTiles.toggle('scale');
+    });
+
+
+});
