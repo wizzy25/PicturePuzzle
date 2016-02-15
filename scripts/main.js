e5e84a69adef2233bc1eccff8037ef6e56a648e4
Williamso <womayuku@gmail.com>
2016-02-15 22:12:33 +0100

    Changed my table to DIVs, Tried to make elements stay ontop

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@
  */
 $(function() {
 
-    var gameTiles = $('td > div');
-    gameTiles.addClass('gameTiles');
+    var gameTiles = $('.gameTiles');
+
     var win = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36];
 
     //INITIALIZE THE DRAGGABLES
@@ -12,6 +12,10 @@ $(function() {
     gameTiles.draggable('option', 'opacity', 0.35);
     gameTiles.draggable('option', 'delay', 500);
 
+    //INITIALIZE THE DROPPABLE TILES
+    gameTiles.droppable({
+        accept: 'td'
+    });
 
     gameTiles.on('dragcreate', function() {
         gameTiles.toggle('scale');
@@ -19,3 +23,15 @@ $(function() {
 
 
 });
+
+var ontop = document.getElementsByClassName('gameTiles');
+
+for (var i=0; x=0 < ontop.length; i++) {
+    ontop[i].addEventListener('mousedown', function() {
+        var prev = this;
+        if (prev) {
+            prev.style.zIndex = ontop.length+1;
+        }
+        this.style.zIndex = ontop.length+2;
+    }, false)
+}
